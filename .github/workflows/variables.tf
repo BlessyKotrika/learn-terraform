51a262b28c170a67a0d3eb231b23c872d3867703
BlessyKotrika <Blessy.Kotrika@geappliances.com>
2024-09-02 01:12:40 +0530

    Changes for TF

---
+++ .github/workflows/variables.tf
@@ -0,0 +1,3 @@
+variable "my_aws_access_key" {}
+
+variable "my_aws_access_secret" {}
--- main.tf
+++ main.tf
@@ -1,31 +1,58 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 provider "aws" {
-  region = var.region
+  region = "us-east-2"  # Change to your preferred region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+# Create an S3 bucket
+resource "aws_s3_bucket" "my_bucket"{
+  bucket = "my-unique-bucket-name-blessy"  # Change to your preferred bucket name
 
   tags = {
-    Name = var.instance_name
+    Name        = "My bucket"
+    Environment = "Dev"
   }
+}
+
+# Enable versioning
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "terraform-lambda-execution-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# Archive a single file.
+
+data "archive_file" "lambda_handler" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_handler.py"
+  output_path = "${path.module}/lambda_handler.zip"
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  filename         = "lambda_handler.zip"
+  function_name    = "myTerraformLambdaFunction"
+  role             = aws_iam_role.lambda_execution_role.arn
+  handler          = "lambda_handler.lambda_handler"
+  runtime          = "python3.8"
+  timeout          = 60
+  depends_on       = [aws_iam_role.lambda_execution_role, data.archive_file.lambda_handler]
 }
